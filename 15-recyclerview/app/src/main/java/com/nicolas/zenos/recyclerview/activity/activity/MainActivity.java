d70569fe6fd18f2e194bba1c8c0e00b326bd951c
Nícolas Teixeira <nicolasteixeira3856@outlook.com>
2021-02-19 13:42:02 -0300

    RecyclerView

--- 15-recyclerview/app/src/main/java/com/nicolas/zenos/recyclerview/activity/activity/MainActivity.java
+++ 15-recyclerview/app/src/main/java/com/nicolas/zenos/recyclerview/activity/activity/MainActivity.java
@@ -1,12 +1,20 @@
 package com.nicolas.zenos.recyclerview.activity.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.nicolas.zenos.recyclerview.R;
+import com.nicolas.zenos.recyclerview.activity.RecyclerItemClickListener;
 import com.nicolas.zenos.recyclerview.activity.adapter.Adapter;
 import com.nicolas.zenos.recyclerview.activity.model.Movie;
 
@@ -32,11 +40,48 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+        recyclerView.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerView,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Movie movie = listMovies.get(position);
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Item click: " + movie.getMovieTitle(),
+                                        Toast.LENGTH_LONG
+                                ).show();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Movie movie = listMovies.get(position);
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Long click: " + movie.getMovieTitle(),
+                                        Toast.LENGTH_LONG
+                                ).show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Toast.makeText(
+                                        getApplicationContext(),
+                                        "Item pressionado",
+                                        Toast.LENGTH_LONG
+                                ).show();
+                            }
+                        }
+                )
+        );
         recyclerView.setAdapter(adapter);
     }
 
     public void createMovies() {
-        Movie movie = new Movie("Senhor dos Anéis - A Sociedaed do Anel", "Fantasia", "2001");
+        Movie movie = new Movie("Senhor dos Anéis - A Sociedade do Anel", "Fantasia", "2001");
         this.listMovies.add(movie);
 
         movie = new Movie("Senhor dos Anéis - As Duas Torres", "Fantasia", "2002");
--- 15-recyclerview/app/src/main/java/com/nicolas/zenos/recyclerview/activity/adapter/Adapter.java
+++ 15-recyclerview/app/src/main/java/com/nicolas/zenos/recyclerview/activity/adapter/Adapter.java
@@ -40,7 +40,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     @Override
     public int getItemCount() {
-        return 20;
+        return 6;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
